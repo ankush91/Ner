d4159f51f7f3e82972922017bfc7e05fc475c048
ankush91 <ankushisraney@gmail.com>
2016-12-05 11:02:34 -0800

    some command-line options

--- code/control.py
+++ code/control.py
@@ -4,35 +4,73 @@ import testPreprocess
 import trainModel
 import testRun
 import testAnnotatePreprocess
+import sys, getopt
 
-def genericLoop():
-    '''
-    *****MAIN CONTROL LOOP FOR THE ML TASK*****
-    '''
+#printing usage for command line options and exit
+def printUsage():
+    print ('----USAGE----')
+    print ('python control.py -pre_train_skip -train_skip -test_skip -n_train <number>')
+    sys.exit(2)
+
+#Command Line options for System
+def command_line_opts(argv):
+    #Try if options are present
     
+    list = []
+    
+    #Iterate through list of arguments (Only these arguments would be matched if size of list is <=6
+    if len(argv) <= 6:
+        for id,  arg   in enumerate(argv):
+            if arg == '-pre_train_skip':
+                list.append(arg)
+            elif arg == '-train_skip':
+                list.append(arg)
+            elif arg ==  '-test_skip':
+                list.append(arg)
+            elif arg == '-train_n' :
+                list.append('-train_n')
+                if str(argv[id+1]).isdigit():
+                    list.append(argv[id+1])
+                else:
+                    printUsage()
+                    sys.exit(2)
+    
+    #Return List
+    return list
+                
+def genericLoop(argv):
+    
+   #*****MAIN CONTROL LOOP FOR THE ML TASK*****
+   
+   #get Command Line Options
+    options = command_line_opts(argv)
+   
     #Train Preprocess - Parse in .col Format
-    #trainPreprocess.parseTimeML("data/TE3-Silver-data/TE3-Silver-data")
-    list = trainPreprocess.inputCol();
+    if "-pre_train_skip" not in options:
+        trainPreprocess.parseTimeML("data/TE3-Silver-data/TE3-Silver-data")
     
-    #Train Model using Stanford Core-nlp
-    trainModel.train(list)
-     
-    #Preprocess Test Model in .txt format
-    testPreprocess.preProcess()
+    if "-train_skip" not in options:
+        list = trainPreprocess.inputCol();
+        #Train Model using Stanford Core-nlp
+        trainModel.train(list)
     
-    #Preprocess Test in .col format
-    testAnnotatePreprocess.parseTimeML("data/te3-platinum-data")
-
-    #run the learnt models on the Test-data
-    testRun.test()
+    if "-test_skip" not in options:
+        #Preprocess Test Model in .txt format
+        testPreprocess.preProcess()
+        #Preprocess Test in .col format
+        testAnnotatePreprocess.parseTimeML("data/te3-platinum-data")
+        #run the learnt models on the Test-data
+        testRun.test()
+    
  
 #-----------MAIN FUNCTION---------- 
-def main():
-    
-    
+def main(argv):
     #****CALL to START CONTROL LOOP****
-    genericLoop()
+    genericLoop(argv)
     
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
+
+
+    
